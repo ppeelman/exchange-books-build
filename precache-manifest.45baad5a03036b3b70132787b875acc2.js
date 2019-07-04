74fa2808fbba0fe1629737d349e547c61d6fd8ae
ppeelman <philippe.peelman@gmail.com>
2019-07-04 12:19:10 +0200

    First build

---
+++ precache-manifest.45baad5a03036b3b70132787b875acc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15933ec87d2f8f8b1f320208b5fe79a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd54bfa7a7953d0bb0b4",
+    "url": "/static/css/main.654375f2.chunk.css"
+  },
+  {
+    "revision": "624cd688107e7b91e740",
+    "url": "/static/js/2.771af96e.chunk.js"
+  },
+  {
+    "revision": "cd54bfa7a7953d0bb0b4",
+    "url": "/static/js/main.ab1a3ca3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7d4dd3bc5ab9116fb91a1860d3628a2",
+    "url": "/static/media/buddhism.b7d4dd3b.jpg"
+  },
+  {
+    "revision": "d2bfbc2469fa5f40dd432dea38e2cbcd",
+    "url": "/static/media/isabelle.d2bfbc24.jpg"
+  },
+  {
+    "revision": "bd1ef12285f53177a0ddee5cde750b64",
+    "url": "/static/media/sapiens.bd1ef122.jpg"
+  }
+]);
